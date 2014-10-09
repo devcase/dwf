8eae497ffd8438608a5026a6a169dd09486241dd
hirata <hirata@7f984ae3-0058-46c0-b942-99a1989fe96d>
2014-10-09 17:30:15 +0000

    git-svn-id: http://192.168.1.133:12667/svn/dwf/dwf/trunk@65 7f984ae3-0058-46c0-b942-99a1989fe96d

--- dwf/dwf-core/src/main/java/dwf/persistence/embeddable/Price.java
+++ dwf/dwf-core/src/main/java/dwf/persistence/embeddable/Price.java
@@ -7,6 +7,7 @@ import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.persistence.Transient;
 
+import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
 
 @Embeddable
@@ -52,7 +53,17 @@ public class Price implements Serializable {
 
 	@Override
 	public String toString() {
-		return new StringBuilder().append(currencyCode).append(": ").append(value).toString();
+		return new StringBuilder().append(currencyCode).append(" ").append(value).toString();
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if(obj != null && obj instanceof Price) {
+			return ObjectUtils.compare(((Price) obj).value, this.value) == 0 && ObjectUtils.compare(((Price) obj).currencyCode, this.currencyCode) == 0;
+		}
+		return false;
 	}	
+	
 	
 }
