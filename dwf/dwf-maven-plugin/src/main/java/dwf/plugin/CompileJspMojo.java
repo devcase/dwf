38f141da570b25a8850153aaef2d382b7a64b8fe
Thiago Hirata <hirata@devcase.com.br>
2015-04-22 14:48:24 -0300

    dwf-parent - ajustes

--- dwf/dwf-maven-plugin/src/main/java/dwf/plugin/CompileJspMojo.java
+++ dwf/dwf-maven-plugin/src/main/java/dwf/plugin/CompileJspMojo.java
@@ -7,9 +7,16 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 
 import dwf.tools.jspc.DwfJspC;
 
+/**
+ * Transforms jsp into java servlets, creates a class that maps each servlet for its
+ * original path and adds the sources to the maven project
+ * @author Hirata
+ *
+ */
 @Mojo(name="compile-jsp")
 public class CompileJspMojo extends AbstractMojo {
 	@Parameter(name="uriRoot", required=true)
@@ -18,6 +25,8 @@ public class CompileJspMojo extends AbstractMojo {
 	private String outputDir;
 	@Parameter(name="targetPackage", required=true)
 	private String targetPackage;
+	@Parameter(defaultValue="${project}")
+    private MavenProject project;
 	
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
@@ -26,8 +35,14 @@ public class CompileJspMojo extends AbstractMojo {
 			jspc.setUriroot(this.uriRoot);
 			jspc.setOutputDir(this.outputDir);
 			jspc.targetPackage = this.targetPackage;
+			
+            this.getLog().info( "Compiling jsps." );      
 			jspc.execute();
+            this.getLog().info( "Generating GeneratedJspConfiguration.java");      
 			jspc.generate();
+			
+			this.project.addCompileSourceRoot( outputDir );
+            this.getLog().info( "Source directory: " + outputDir + " added." );      
 		} catch (IOException e) {
 			throw new MojoExecutionException("Error!", e);
 		}
