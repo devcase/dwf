418228e4f6caff335d7fe18fdb9b7afadefc471c
Henrique Kano <henriquekano@hotmail.com>
2015-08-18 17:03:22 -0300

    retirando mudanças desnecessarias

--- dwf/dwf-data/src/main/java/dwf/persistence/embeddable/WeekdayTime.java
+++ dwf/dwf-data/src/main/java/dwf/persistence/embeddable/WeekdayTime.java
@@ -12,8 +12,6 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonView;
 
