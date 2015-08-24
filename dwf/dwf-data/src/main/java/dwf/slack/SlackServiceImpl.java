8be2bc9a20f0a7bbbcdeccd5ff7aef30edfd5159
Thiago Hirata <hirata@devcase.com.br>
2015-08-24 16:59:00 -0300

    Ajuste na integração com Slack

--- dwf/dwf-data/src/main/java/dwf/slack/SlackServiceImpl.java
+++ dwf/dwf-data/src/main/java/dwf/slack/SlackServiceImpl.java
@@ -1,15 +1,20 @@
 package dwf.slack;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
-
-import dwf.slack.message.PostMessageRequest;
 
 @Component(value="slackService")
 @ConditionalOnProperty(prefix="slack", name="apitoken", matchIfMissing=false)
 public class SlackServiceImpl implements SlackService {
+	private Log log = LogFactory.getLog(getClass());
+	
 	@Value("${slack.apitoken}")
 	private String slackApiToken;
 	@Value("${slack.apiurl:https://slack.com/api/}")
@@ -31,7 +36,12 @@ public class SlackServiceImpl implements SlackService {
 	
 	public void postMessage(String channel, String text) {
 		RestTemplate restTemplate = new RestTemplate();
-		restTemplate.postForEntity(slackApiUrl + "chat.postMessage", new PostMessageRequest(slackApiToken, channel, text), String.class);
+		MultiValueMap<String, Object> request = new LinkedMultiValueMap<String, Object>();
+		request.add("token", slackApiToken);
+		request.add("text", text);
+		request.add("channel", channel);
 		
+		ResponseEntity<String> resp = restTemplate.postForEntity(slackApiUrl + "chat.postMessage", request, String.class);
+		log.info("Response got from SLACK: " + resp);
 	}
 }
