86015db4c383f394c0320a79190c329713f64e05
Thiago Hirata <hirata@devcase.com.br>
2015-12-22 13:31:07 -0200

    dwf-autoreload - configuracões de timeout e efeitos especiais

--- dwf/dwf-web-view/src/main/resources/dwf/web-resources/js/dwf-autoreload.js
+++ dwf/dwf-web-view/src/main/resources/dwf/web-resources/js/dwf-autoreload.js
@@ -11,23 +11,41 @@ $(document).on('dwf-postupdate', function(evt) {
 	autoreloadelements.each(function() {
 	    var $this   = $(this);
 	    var href    = $this.attr('autoreload-href');
+	    var timeout = $this.attr('autoreload-timeout');
+	    if(timeout) {
+	    	timeout = parseInt(timeout);
+	    } else {
+	    	timeout = 5000;
+	    }
+	    var fx = $this.attr('autoreload-fx');
+	    if(fx) {
+	    	fx = "true" == fx;
+	    } else {
+	    	fx = true;
+	    }
 
 		window.setTimeout(function() {
 			if(jQuery.contains(document.documentElement, $this[0])) {
 			    var $wrapper   = $this.wrap('<div class="autoreload-wrapper"></div>').parent();
-				$wrapper.css({'position': 'relative'});
-				$wrapper.append('<div style="position:absolute; top: 45%; left: 20%; right: 20%; z-index: 1000;"><div class="panel panel-default"><div class="panel-body"><div class="progress no-margin"><div class="progress-bar progress-bar-striped active"  role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">Carregando</div></div></div></div></div>');
+			    if(fx) {
+					$wrapper.css({'position': 'relative'});
+					$wrapper.append('<div style="position:absolute; top: 45%; left: 20%; right: 20%; z-index: 1000;"><div class="panel panel-default"><div class="panel-body"><div class="progress no-margin"><div class="progress-bar progress-bar-striped active"  role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">Carregando</div></div></div></div></div>');
+			    }
 			    $.ajax({
 			    	url: href,
 			    	success: function(data) {
 			    		$wrapper.fadeOut({
+			    			duration: fx ? 400 : 0,
 							always: function() {
 								$wrapper.empty();
 								$wrapper.html(data);
 			    	    		$wrapper.fadeIn({
+			    	    			duration: fx ? 400 : 0,
 			    	    			always: function() {
 					    	    		var top = $wrapper.scrollTop(); //Getting Y of target element
-					    	    	    window.scrollTo(0, top);
+					    	    		if(fx) {
+						    	    	    window.scrollTo(0, top);
+					    	    		}
 					    	    	    newcontent = $wrapper.children().unwrap();
 					    	    	    $(newcontent).trigger('dwf-postupdate');
 			    	    			}
@@ -37,6 +55,6 @@ $(document).on('dwf-postupdate', function(evt) {
 			    	}
 			    });
 			}
-		}, 5000);
+		}, timeout);
 	});
 });
