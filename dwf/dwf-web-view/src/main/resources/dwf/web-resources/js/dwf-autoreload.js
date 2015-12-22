ee90cdc1524b31a0580c29819df4a742c962851e
Thiago Hirata <hirata@devcase.com.br>
2015-12-22 11:55:21 -0200

    arrumando dwf-autoreload

--- dwf/dwf-web-view/src/main/resources/dwf/web-resources/js/dwf-autoreload.js
+++ dwf/dwf-web-view/src/main/resources/dwf/web-resources/js/dwf-autoreload.js
@@ -1,13 +1,20 @@
 /**
  */
-$(document).on('dwf-postupdate', function() {
-	$(this).find('[dwf-toggle="autoreload"][autoreload-href]').each(function() {
+$(document).on('dwf-postupdate', function(evt) {
+	var autoreloadelements;
+	if($(evt.target).attr('dwf-toggle') == 'autoreload') {
+		autoreloadelements = $(evt.target);
+	} else {
+		autoreloadelements =$(evt.target).find('[dwf-toggle="autoreload"][autoreload-href]');
+	}
+	
+	autoreloadelements.each(function() {
 	    var $this   = $(this);
 	    var href    = $this.attr('autoreload-href');
-	    var $wrapper   = $(this).wrap('<div class="autoreload-wrapper"></div>').parent();
 
 		window.setTimeout(function() {
-			if(jQuery.contains(document.documentElement, $wrapper[0])) {
+			if(jQuery.contains(document.documentElement, $this[0])) {
+			    var $wrapper   = $this.wrap('<div class="autoreload-wrapper"></div>').parent();
 				$wrapper.css({'position': 'relative'});
 				$wrapper.append('<div style="position:absolute; top: 45%; left: 20%; right: 20%; z-index: 1000;"><div class="panel panel-default"><div class="panel-body"><div class="progress no-margin"><div class="progress-bar progress-bar-striped active"  role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">Carregando</div></div></div></div></div>');
 			    $.ajax({
@@ -19,10 +26,10 @@ $(document).on('dwf-postupdate', function() {
 								$wrapper.html(data);
 			    	    		$wrapper.fadeIn({
 			    	    			always: function() {
-					    	    		$wrapper.trigger('dwf-postupdate');
 					    	    		var top = $wrapper.scrollTop(); //Getting Y of target element
 					    	    	    window.scrollTo(0, top);
-					    	    	    $wrapper.children().unwrap();
+					    	    	    newcontent = $wrapper.children().unwrap();
+					    	    	    $(newcontent).trigger('dwf-postupdate');
 			    	    			}
 			    	    		});
 							}
