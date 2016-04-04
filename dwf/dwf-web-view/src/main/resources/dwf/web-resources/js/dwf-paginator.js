7bf09e47981aaadf3d8f2671e4135137ca9d56c6
Thiago Hirata <hirata@devcase.com.br>
2016-04-04 17:47:03 -0300

    Remoção do scroll do paginador em casos desnecessários

--- dwf/dwf-web-view/src/main/resources/dwf/web-resources/js/dwf-paginator.js
+++ dwf/dwf-web-view/src/main/resources/dwf/web-resources/js/dwf-paginator.js
@@ -13,7 +13,10 @@ $(document).on('click', '[dwf-toggle~="paginator"]', function (evt) {
     evt.preventDefault();
     
 	var top = $wrapper.offset().top; //Getting Y of target element
-	$('body').animate({scrollTop:top}, 200);
+	if($('body').scrollTop() > top) {
+		$('body').animate({scrollTop:top}, 200);
+	}
+
     $wrapper.css({'position': 'relative'});
     $wrapper.append('<div style="position:absolute; top: 45%; left: 20%; right: 20%; z-index: 1000;"><div class="panel panel-default"><div class="panel-body"><div class="progress no-margin"><div class="progress-bar progress-bar-striped active"  role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">Carregando</div></div></div></div></div>');
     $.ajax({
