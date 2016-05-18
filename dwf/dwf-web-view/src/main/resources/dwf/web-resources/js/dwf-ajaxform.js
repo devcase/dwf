7a4c496ec4f184dc8d1a1c061e41f3050802ac24
Thiago Hirata <hirata@devcase.com.br>
2016-05-18 14:59:00 -0300

    Integração entre ajaxform e remoteload

--- dwf/dwf-web-view/src/main/resources/dwf/web-resources/js/dwf-ajaxform.js
+++ dwf/dwf-web-view/src/main/resources/dwf/web-resources/js/dwf-ajaxform.js
@@ -3,6 +3,7 @@
 $(document).on('submit', 'form[dwf-toggle~="ajaxform"]', function(evt) {
 	evt.preventDefault();
 	$this = $(this);
+    var $parent = $($this.parent());
     var $wrapper   = $this.wrap('<div class="ajaxform-wrapper"></div>').parent();
 	$wrapper.css({'position': 'relative'});
 	$wrapper.append('<div style="position:absolute; top: 45%; left: 20%; right: 20%; z-index: 1000;"><div class="panel panel-default"><div class="panel-body"><div class="progress no-margin"><div class="progress-bar progress-bar-striped active"  role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">Carregando</div></div></div></div></div>');
@@ -20,6 +21,13 @@ $(document).on('submit', 'form[dwf-toggle~="ajaxform"]', function(evt) {
 		    	    		var top = $wrapper.scrollTop(); //Getting Y of target element
 		    	    	    window.scrollTo(0, top);
 		    	    	    newcontent = $wrapper.children().unwrap();
+		    	    	    
+		    	    	    //enable links with dwf-toggle="remoteload"
+		    	    	    if($parent.attr('id')) {
+		    	    	    	$(newcontent).find('[dwf-toggle~="remoteload"]').not('[dwf-target]').attr('dwf-target', '#' + $parent.attr('id'))
+		    	    	    }
+		    	    	    
+		    	    	    
 		    	    	    $(newcontent).trigger('dwf-postupdate');
     	    			}
     	    		});
