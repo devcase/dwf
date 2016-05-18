fecdb9cc9caedcbadf47810cde81aac2ce92ef8f
Thiago Hirata <hirata@devcase.com.br>
2016-05-18 19:31:58 -0300

    Melhoria no @RequestParamBean (validação)

--- dwf/dwf-web-rest/src/main/java/dwf/web/rest/spring/RequestParamBeanArgumentResolver.java
+++ dwf/dwf-web-rest/src/main/java/dwf/web/rest/spring/RequestParamBeanArgumentResolver.java
@@ -1,5 +1,6 @@
 package dwf.web.rest.spring;
 
+import java.lang.annotation.Annotation;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -10,8 +11,10 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.MutablePropertyValues;
 import org.springframework.core.MethodParameter;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -47,18 +50,36 @@ public class RequestParamBeanArgumentResolver implements HandlerMethodArgumentRe
 				params.put(n.substring(paramName.length() + 1), webRequest.getParameterValues(n));
 			}
 		}
-		if(params.isEmpty()) return null;
 		
 		Object target = BeanUtils.instantiateClass(parameter.getParameterType());
 		WebDataBinder dataBinder = binderFactory.createBinder(webRequest, target, methodArgumentName);
 		MutablePropertyValues mpvs = new MutablePropertyValues(params);
 		dataBinder.bind(mpvs);
-		if(dataBinder.getBindingResult().hasErrors()) {
-			BindingResult bindingResult = dataBinder.getBindingResult();
-			log.warn("Erro ao fazer o binding:" + bindingResult.getAllErrors());
-		}
+		validateIfApplicable(dataBinder, parameter);
+		
+		mavContainer.addAttribute(methodArgumentName, target);
+		mavContainer.addAttribute(BindingResult.MODEL_KEY_PREFIX + methodArgumentName, dataBinder.getBindingResult());
 		return target;
 	}
 
-	
+	/**
+	 * Validate the model attribute if applicable.
+	 * <p>The default implementation checks for {@code @javax.validation.Valid},
+	 * Spring's {@link org.springframework.validation.annotation.Validated},
+	 * and custom annotations whose name starts with "Valid".
+	 * @param binder the DataBinder to be used
+	 * @param methodParam the method parameter
+	 */
+	protected void validateIfApplicable(WebDataBinder binder, MethodParameter methodParam) {
+		Annotation[] annotations = methodParam.getParameterAnnotations();
+		for (Annotation ann : annotations) {
+			Validated validatedAnn = AnnotationUtils.getAnnotation(ann, Validated.class);
+			if (validatedAnn != null || ann.annotationType().getSimpleName().startsWith("Valid")) {
+				Object hints = (validatedAnn != null ? validatedAnn.value() : AnnotationUtils.getValue(ann));
+				Object[] validationHints = (hints instanceof Object[] ? (Object[]) hints : new Object[] {hints});
+				binder.validate(validationHints);
+				break;
+			}
+		}
+	}
 }
